af6b005c97438bc58f706229f8fc1555fb487321
Takosuke <takosuke.g@gmail.com>
2018-06-18 23:46:29 +0900

    commit

---
+++ exporter.py
@@ -0,0 +1,180 @@
+import math
+import svgwrite
+import bpy
+import logging
+import io
+import mathutils
+import operator
+
+logger = logging.getLogger("svg_exporter")
+
+class SvgExporter(bpy.types.Operator):
+    bl_idname = "svg.exporter"
+    bl_label = "Export SVG"
+
+    def __init__(self):
+        logger.info("start")
+
+        self.objs = []
+
+        logger.info("end")
+
+    def invoke(self, context, event):
+        logger.info("start")
+
+        svg_scene_properties = context.scene.svg_scene_properties
+        export_path = bpy.path.abspath(svg_scene_properties.export_path)
+        width = svg_scene_properties.width
+        height = svg_scene_properties.height
+
+        self.svg = svgwrite.Drawing(filename=export_path, size=(width,height), profile='tiny')
+        self.svg.viewbox(minx=-width/2, miny=-height/2, width=width, height=height)
+
+        if svg_scene_properties.use_background:
+            background_color = svg_scene_properties.background_color
+            rect = self.svg.rect(insert=(-width/2, -height/2), size=('100%', '100%'), rx=None, ry=None, fill=self.get_color(background_color), opacity=background_color[3])
+            self.svg.add(rect)
+
+        self.get_objects()
+        self.sort_objects()
+        self.add_objects_data()
+
+        self.svg.save()
+
+        logger.info("end")
+
+        return {'FINISHED'}
+
+    def get_objects(self):
+        for obj in bpy.data.objects:
+            if not obj.is_visible(bpy.context.scene):
+                continue
+
+            if obj.type != 'CURVE':
+                continue
+
+            curve = obj.data
+
+            if curve.dimensions != '2D':
+                logger.info("This curve is not 2D: " + str(obj.name))
+                continue
+
+            if len(curve.materials) <= 0:
+                logger.info("This data has no material: " + str(obj.name))
+                continue
+
+            material = curve.materials[0]
+
+            if material is None:
+                logger.info("This material slot has no material: " + str(obj.name))
+                return
+
+            self.objs.append(obj)
+
+    def sort_objects(self):
+        self.objs.sort(key=lambda obj: obj.location[2])
+
+    def add_objects_data(self):
+        for obj in self.objs:
+            self.add_curve_data(obj)
+
+    def add_curve_data(self, obj):
+        logger.debug("add data: " + str(obj.name))
+        color = self.get_diffuse_color(obj)
+        alpha = self.get_alpha(obj)
+        curve = obj.data
+        matrix_world = obj.matrix_world
+        scale = bpy.context.scene.svg_scene_properties.scale
+
+        for spline in curve.splines:
+            if spline.type != 'BEZIER':
+                logger.info("Spline type is not BEZIER")
+                continue
+
+            # ループしているかの判定。使うかどうか検討中。
+            is_loop = spline.use_cyclic_u
+
+            svg_path = SVGPath(spline, matrix_world, scale)
+
+            self.svg.add(self.svg.path(d=svg_path.d, fill=color, opacity=alpha, stroke=color))
+
+    def get_diffuse_color(self, obj):
+        material = obj.data.materials[0]
+        diffuse_color = material.diffuse_color
+        return self.get_color(diffuse_color)
+
+    def get_alpha(self, obj):
+        material = obj.data.materials[0]
+        return material.alpha
+
+    def get_color(self, color):
+        gamma = 2.2
+        r = 255 * pow(color[0], 1/gamma)
+        g = 255 * pow(color[1], 1/gamma)
+        b = 255 * pow(color[2], 1/gamma)
+        return svgwrite.rgb(r,g,b)
+
+class SVGPath():
+    svg_matrix = mathutils.Matrix((
+        [1.0, 0.0, 0.0, 0.0],
+        [0.0, -1.0, 0.0, 0.0],
+        [0.0, 0.0, 1.0, 0.0],
+        [0.0, 0.0, 0.0, 1.0]))
+
+    def __init__(self, spline, matrix_world, scale):
+        self.spline = spline
+        self.matrix_world = matrix_world
+        self.scale = scale
+        self.ds = []
+
+        self.append_move_to()
+        self.append_bezier_curve()
+        self.append_end()
+
+        self.d = ' '.join(self.ds)
+
+    def append_move_to(self):
+        m = self.get_global_pos(self.spline.bezier_points[0].co)
+        self.ds.append("M{0},{1}".format(m.x, m.y))
+
+    def append_bezier_curve(self):
+        for i in range(len(self.spline.bezier_points) - 1):
+            p1 = self.spline.bezier_points[i]
+            p2 = self.spline.bezier_points[i + 1]
+
+            c1 = self.get_global_pos(p1.handle_right)
+            c2 = self.get_global_pos(p2.handle_left)
+            c = self.get_global_pos(p2.co)
+
+            self.ds.append("C {0},{1} {2},{3} {4},{5}".format(c1.x, c1.y, c2.x, c2.y, c.x, c.y))
+
+    def append_end(self):
+        start_point = self.spline.bezier_points[0]
+        end_point = self.spline.bezier_points[-1]
+
+        c1 = self.get_global_pos(end_point.handle_right)
+        c2 = self.get_global_pos(start_point.handle_left)
+        c = self.get_global_pos(start_point.co)
+
+        self.ds.append("C {0},{1} {2},{3} {4},{5}".format(c1.x, c1.y, c2.x, c2.y, c.x, c.y))
+
+    def get_global_pos(self, vec):
+        v = vec.copy()
+        v.resize_4d()
+
+        w = self.svg_matrix * self.matrix_world * v
+        w = w * self.scale
+        w.resize_3d()
+        return w
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ properties.py
@@ -0,0 +1,240 @@
+import bpy
+import logging
+import itertools
+import math
+import mathutils
+import os
+import bgl
+from . exporter import SvgExporter
+from bpy.props import PointerProperty, StringProperty, CollectionProperty, IntProperty, BoolProperty, IntVectorProperty, FloatVectorProperty, FloatProperty, EnumProperty, BoolVectorProperty
+from bpy.app.translations import pgettext
+from bpy.types import Panel, Operator, SpaceView3D, PropertyGroup
+
+logger = logging.getLogger("svg_exporter")
+
+# Properties
+class SVGSceneProperties(PropertyGroup):
+    height = IntProperty(name="Height", min=4, max=65536, default=1080)
+    width = IntProperty(name="Width", min=4, max=65536, default=1920)
+    scale = FloatProperty(name="Scale", min=0.00001, max=100000.0, step=1, default=100.0, precision=3)
+    export_path = StringProperty(name="Export path", subtype='FILE_PATH', description="Export path", default="//sample.svg")
+    draw_area = BoolProperty(default=False)
+    slide = FloatProperty(name="Slide", step=10, default=0.1)
+    use_background = BoolProperty(name="Use backGround", default=False)
+    background_color = FloatVectorProperty(name="Background Color", subtype='COLOR', size=4, min=0, max=1, default=[0.8, 0.8, 0.8, 0.8])
+
+
+# UI
+class SVGToolPanel(Panel):
+    bl_idname = "OBJECT_PT_svg"
+    bl_label = "SVG Exporter"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'TOOLS'
+    bl_category = 'SVG'
+
+    def draw(self, context):
+        svg_scene_properties = context.scene.svg_scene_properties
+
+        layout = self.layout
+
+        row = layout.row()
+
+        if svg_scene_properties.draw_area is False:
+            icon = 'PLAY'
+            txt = 'Display border'
+        else:
+            icon = "PAUSE"
+            txt = 'Hide border'
+
+        row.operator("svg.runopenglbutton", text=txt, icon=icon)
+
+        # layout.prop(svg_scene_properties, "property_type", expand=True)
+        col = layout.column(align=True)
+        col.prop(svg_scene_properties, "height")
+        col.prop(svg_scene_properties, "width")
+        col.prop(svg_scene_properties, "scale")
+
+        row = layout.row()
+        row.prop(svg_scene_properties, "use_background", text="Use background")
+
+        row = layout.row()
+        if svg_scene_properties.use_background:
+            row.prop(svg_scene_properties, "background_color", text="")
+
+        row = layout.row()
+        row.label("Export Path")
+        row = layout.row()
+        row.prop(svg_scene_properties, "export_path", text="")
+        row = layout.row()
+        row.operator(OpenSvg.bl_idname, icon='FILE_FOLDER')
+
+        layout.row().separator()
+
+        row = layout.row()
+        row.operator(AddCurveTool.bl_idname, icon='CURVE_BEZCIRCLE')
+
+        col = layout.column(align=True)
+        row = col.row(align=True)
+        row.operator(UpObject.bl_idname, icon='TRIA_UP')
+        row.operator(DownObject.bl_idname, icon='TRIA_DOWN')
+        col.prop(svg_scene_properties, "slide")
+        col.operator(ResetObject.bl_idname, icon='X')
+
+        layout.row().separator()
+        
+        if context.object is not None:
+            obj = context.object
+            if obj.type == 'CURVE':
+                row = layout.row()
+                row.prop(obj.data, "resolution_u")
+            if len(obj.data.materials) > 0:
+                mat = obj.data.materials[0]
+                col = layout.column(align=True)
+                col.label("Viewport Color:")
+                col.prop(mat, "diffuse_color", text="")
+                col.prop(mat, "alpha")
+
+        layout.row().separator()
+
+        row = layout.row()
+        row.scale_y = 2.0
+        row.operator(SvgExporter.bl_idname, icon='EXPORT')
+
+# op
+class AddCurveTool(Operator):
+    bl_idname = "svg.addcurve"
+    bl_label = "Add curve"
+
+    def invoke(self, context, event):
+        bpy.ops.curve.primitive_bezier_circle_add(location=(0.0, 0.0, 0.0))
+        obj = context.object
+
+        obj.lock_location[2] = True
+
+        curve = obj.data
+
+        curve.dimensions = '2D'
+        curve.resolution_u = 5
+
+        mat = bpy.data.materials.new(name="svg_material")
+        mat.diffuse_color = (1.0, 1.0, 1.0)
+        curve.materials.append(mat)
+
+        return {'FINISHED'}
+
+class UpObject(Operator):
+    bl_idname = "svg.upobject"
+    bl_label = "Up"
+
+    def invoke(self, context, event):
+        slide = context.scene.svg_scene_properties.slide
+        for obj in context.selected_objects:
+            obj.location[2] += slide
+
+        return {'FINISHED'}
+
+class DownObject(Operator):
+    bl_idname = "svg.downobject"
+    bl_label = "Down"
+
+    def invoke(self, context, event):
+        slide = context.scene.svg_scene_properties.slide
+        for obj in context.selected_objects:
+            obj.location[2] -= slide
+
+        return {'FINISHED'}
+
+class ResetObject(Operator):
+    bl_idname = "svg.resetobject"
+    bl_label = "Reset"
+
+    def invoke(self, context, event):
+        for obj in context.selected_objects:
+            obj.location[2] = 0.0
+
+        return {'FINISHED'}
+
+class OpenSvg(Operator):
+    bl_idname = "svg.opensvg"
+    bl_label = "Open SVG"
+
+    def invoke(self, context, event):
+        file_path = bpy.path.abspath(context.scene.svg_scene_properties.export_path)
+        try: bpy.ops.wm.url_open(url=file_path)
+        except: pass
+        return{'FINISHED'}
+
+        return {'FINISHED'}
+
+class RunHintDisplayButton(Operator):
+    bl_idname = "svg.runopenglbutton"
+    bl_label = "Display hint data manager"
+
+    _handle_3d = None
+
+    def invoke(self, context, event):
+        if context.scene.svg_scene_properties.draw_area is False:
+            logger.debug("check 1")
+            if context.area.type == 'VIEW_3D':
+                args = (self, context)
+                RunHintDisplayButton._handle_3d = bpy.types.SpaceView3D.draw_handler_add(draw_callback_3d, args, 'WINDOW', 'POST_VIEW')
+                context.scene.svg_scene_properties.draw_area = True
+                context.area.tag_redraw()
+                return {'FINISHED'}
+            else:
+                self.report({'WARNING'}, "View3D not found, cannot run operator")
+                return {'CANCELLED'}
+        else:
+            logger.debug("check 2")
+            logger.debug(context.scene.svg_scene_properties.draw_area)
+            if RunHintDisplayButton._handle_3d is not None:
+                logger.debug(type(RunHintDisplayButton._handle_3d))
+                bpy.types.SpaceView3D.draw_handler_remove(RunHintDisplayButton._handle_3d, 'WINDOW')
+                context.scene.svg_scene_properties.draw_area = False
+                context.area.tag_redraw()
+            else:
+                context.scene.svg_scene_properties.draw_area = False
+            return {'FINISHED'}
+
+def draw_callback_3d(self, context):
+    bgl.glEnable(bgl.GL_BLEND)
+
+    height = context.scene.svg_scene_properties.height
+    width = context.scene.svg_scene_properties.width
+    scale = context.scene.svg_scene_properties.scale
+
+    draw_line_3d((-width/2/scale, height/2/scale, 0.0), (width/2/scale, height/2/scale, 0.0))
+    draw_line_3d((width/2/scale, height/2/scale, 0.0), (width/2/scale, -height/2/scale, 0.0))
+    draw_line_3d((width/2/scale, -height/2/scale, 0.0), (-width/2/scale, -height/2/scale, 0.0))
+    draw_line_3d((-width/2/scale, -height/2/scale, 0.0), (-width/2/scale, height/2/scale, 0.0))
+
+    # restore opengl defaults
+    bgl.glLineWidth(1)
+    bgl.glDisable(bgl.GL_BLEND)
+    bgl.glColor4f(0.0, 0.0, 0.0, 1.0)
+
+def draw_line_3d(start, end, width=1):
+    bgl.glLineWidth(width)
+    bgl.glColor4f(1.0, 1.0, 0.0, 0.5)
+    bgl.glBegin(bgl.GL_LINES)
+    bgl.glVertex3f(*start)
+    bgl.glVertex3f(*end)
+    bgl.glEnd()
+
+translations = {
+    "ja_JP": {
+        ("*", "Base Settings"): "基本設定",
+        ("*", "Export SVG"): "Export SVG",
+        ("*", "Use background"): "背景色を使用",
+    }
+}
+
+def register():
+    bpy.types.Scene.svg_scene_properties = PointerProperty(type=SVGSceneProperties)
+
+    bpy.app.translations.register(__name__, translations)
+
+def unregister():
+    bpy.app.translations.unregister(__name__)
+    del bpy.types.Scene.svg_scene_properties
+
